8e72f5143ebc1b88a5115f97553155e808e9a582
VladosusCocosus <razin36986@gmail.com>
2021-10-23 20:14:19 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+const Tasks = ({ tasks, setTasks }) => {
+    const deleteTask = (id) => {
+        const filteredTasks = tasks.filter((task) => task.id !== id)
+
+        setTasks(filteredTasks)
+    }
+    return (
+        <div className={"tasks"}>
+            {tasks.map((task) => {
+                return (<div className={"task"}>
+                    <span>{task.id}. {task.description}</span>
+                    <button onClick={() => deleteTask(task.id)}>Delete</button>
+                </div>)
+            })}
+        </div>
+    )
+}
+
+const InputTasks = ({ setTasks, tasks }) => {
+    const [description, setDescription] = useState("")
+    const getTask = (_description) => {
+        return {
+            id: tasks.length + 1,
+            description: _description,
+            created_at: new Date()
+        }
+    }
+
+    const createTask = () => {
+        const tasksCopy = [...tasks]
+        tasksCopy.push(getTask(description))
+        setTasks(tasksCopy)
+    }
+    return (
+        <div>
+            <input
+                type="text"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            />
+            <button onClick={() => createTask()}>Save</button>
+        </div>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tasks, setTasks] = useState([])
+    return (
+        <div key={`key-${tasks.length}`}>
+            <Tasks setTasks={setTasks} tasks={tasks}/>
+            <InputTasks setTasks={setTasks} tasks={tasks}/>
+        </div>
+    );
 }
 
 export default App;
